dc92789429a0a86d9c2936d1e16137a246c9ff03
daithioc95 <daithioc@outlook.com>
2021-10-09 13:54:46 +0000

    Implemented SetTasks form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
+import AddTask from "./components/AddTask";
 
 const App = () => {
   const [tasks, setTasks] = useState(
@@ -25,15 +26,34 @@ const App = () => {
         }
     ]
 )
+
+// Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000)+1
+  const newTask = { id, ...task}
+  setTasks([...tasks, newTask])
+}
+
 // Delete Task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
+// Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(
+    tasks.map((task) =>
+      task.id === id ? { ...task, reminder:
+      !task.reminder}:task
+    )
+  )
+}
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      <AddTask onAdd={addTask} />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks to show'}
     </div>
   );
 }
