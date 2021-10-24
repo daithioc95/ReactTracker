115a9e0a896477f261938c5f2db66e7eeff3c400
daithioc95 <daithioc@outlook.com>
2021-10-24 20:26:09 +0000

    Add routing to About section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,35 @@
-import { useState } from "react";
-import Header from "./components/Header"
-import Tasks from "./components/Tasks"
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import About from "./components/About";
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState(
-    [
-        {
-            "day": "Gold Romeo Square Hoops",
-            "id": "85",
-            "text": "Stunning! I love them.",
-            "in_stock": "true"
-        },
-        {
-            "day": "Gold Siren Hoops",
-            "id": "110",
-            "text": "Yummy",
-            "in_stock": "false"
-        },
-        {
-            "day": "Half Moon Pearl Earrings",
-            "id": "70",
-            "text": "Delightful! Dressy without being garish.",
-            "in_stock": "true"
-        }
-    ]
-)
+  const [tasks, setTasks] = useState([
+    {
+      "day": "Gold Romeo Square Hoops",
+      "id": "85",
+      "text": "Stunning! I love them.",
+      "reminder": true
+    },
+    {
+      "day": "Gold Siren Hoops",
+      "id": "110",
+      "text": "Yummy",
+      "reminder": false
+    },
+    {
+      "day": "Half Moon Pearl Earrings",
+      "id": "70",
+      "text": "Delightful! Dressy without being garish.",
+      "reminder": true
+    }
+  ])
+
+  
 
 // Add Task
 const addTask = (task) => {
@@ -51,13 +54,34 @@ const toggleReminder = (id) => {
 }
 
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask (!showAddTask)}
-      showAdd ={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks to show'}
-    </div>
-  );
+    <Router>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask (!showAddTask)}
+        showAdd ={showAddTask} />
+        
+        <Route
+          path='/'
+          exact
+          render={(props) => (
+            <>
+              {showAddTask && <AddTask onAdd={addTask} />}
+              {tasks.length > 0 ? (
+                <Tasks
+                  tasks={tasks}
+                  onDelete={deleteTask}
+                  onToggle={toggleReminder}
+                />
+              ) : (
+                'No Tasks To Show'
+              )}
+            </>
+          )}
+        />
+        <Route path='/about' component={About} />
+        <Footer />
+      </div>
+    </Router>
+  )
 }
 
 export default App;
